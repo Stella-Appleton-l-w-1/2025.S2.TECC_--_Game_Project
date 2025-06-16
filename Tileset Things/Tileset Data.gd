ea5a180fda7ebe4575f284c42c365ed80f23f49d
Daniel-Appleton-123 <stu24299@ashs.school.nz>
2025-06-16 18:34:51 +1200

    Added Tutorial Map
    Added 2 simple tutorial tilesets.

--- Tileset Things/Tileset Data.gd
+++ Tileset Things/Tileset Data.gd
@@ -4,10 +4,15 @@ class_name TilesetData extends Node
 
 #region Tiles
 enum TILE { ## Determines what the numbers in tilesets mean.
-	EMPTY, ## Nothing. Player can exist here.
-	FULL, ## A wall or floor block. Player cannot exist here.
-	CREATE, ## Player's respawn point. Player can exist here.
-	DESTROY, ## Teleports Player to CREATE. Player can exist here.
+	EMPTY = 0, ## Nothing. Player can exist here.
+	FULL = 1, ## A wall or floor block. Player cannot exist here.
+	CREATE = 2, ## Player's respawn point.
+	DESTROY = 3, ## Teleports Player to CREATE.
+	WALK = 4, ## Converts Player to "walk mode".
+	JUMP = 5, ## Converts Player to "jump mode".
+	TURN0 = 6, ## Acts like EMPTY when TurnerValue is false, and FULL otherwise.
+	TURN1 = 7, ## Acts like EMPTY when TurnerValue is true, and FULL otherwise.
+	TURNER = 8, ## Sets TurnerValue to not(TurnerValue).
 }
 #endregion Tiles
 
@@ -43,10 +48,18 @@ func _ready():
 #endregion TEST
 
 #region Tilesets
+enum TSET {
+	EMPTY = 0,
+	FULL = 1,
+	INTRO_WALK = 2,
+	INTRO_JUMP = 3,
+	
+}
+
 var tset_numbers: Dictionary = {
-	0: empty_tset,
-	1: full_tset,
-	2: intro_tset,
+	TSET.EMPTY: empty_tset,
+	TSET.FULL: full_tset,
+	TSET.INTRO_WALK: intro_walk_tset,
 }
 
 var empty_tset: Array[Array]:
@@ -57,15 +70,26 @@ var full_tset: Array[Array]:
 	get:
 		return make_tset(TILE.FULL)
 
-var intro_tset: Array[Array] = [
+var intro_walk_tset: Array[Array] = [
 	[1, 1, 1, 1, 1, 1, 1, 1],
-	[1, 2, 0, 0, 0, 0, 0, 1],
-	[1, 0, 0, 0, 0, 0, 0, 1],
-	[1, 0, 0, 0, 0, 0, 0, 1],
-	[1, 0, 0, 0, 0, 0, 0, 1],
-	[1, 0, 0, 0, 0, 0, 0, 1],
-	[1, 0, 0, 0, 0, 0, 0, 1],
-	[1, 1, 1, 1, 1, 1, 1, 1],
+	[1, 7, 0, 7, 7, 7, 0, 1],
+	[1, 6, 0, 6, 6, 6, 8, 1],
+	[1, 3, 0, 3, 1, 1, 1, 1],
+	[1, 0, 0, 0, 1, 2, 0, 1],
+	[1, 0, 0, 0, 1, 1, 0, 1],
+	[1, 7, 1, 0, 0, 0, 0, 1],
+	[1, 4, 1, 1, 1, 1, 1, 1],
+]
+
+var intro_jump_tset: Array[Array] = [
+	[1, 0, 1, 0, 0, 0, 0, 0],
+	[1, 5, 1, 0, 0, 0, 0, 0],
+	[1, 0, 0, 0, 0, 0, 0, 0],
+	[1, 0, 0, 0, 0, 0, 0, 0],
+	[1, 2, 0, 0, 0, 0, 0, 0],
+	[1, 1, 1, 0, 0, 0, 1, 1],
+	[1, 1, 1, 0, 0, 0, 1, 1],
+	[1, 1, 1, 3, 3, 1, 1, 1],
 ]
 #endregion Tilesets
 
@@ -74,7 +98,7 @@ var intro_tset: Array[Array] = [
 var tset_map: Array[Array] = [
 	[1, 1, 1, 1, 1, 1, 1, 1],
 	[1, 2, 0, 0, 0, 0, 0, 1],
-	[1, 0, 0, 0, 0, 0, 0, 1],
+	[1, 3, 0, 0, 0, 0, 0, 1],
 	[1, 0, 0, 0, 0, 0, 0, 1],
 	[1, 0, 0, 0, 0, 0, 0, 1],
 	[1, 0, 0, 0, 0, 0, 0, 1],
