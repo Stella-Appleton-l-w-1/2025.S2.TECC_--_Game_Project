3b2e4fc618705e8f7c5ccc94684706326587227b
Daniel-Appleton-123 <stu24299@ashs.school.nz>
2025-06-16 16:10:43 +1200

    Testing what I have so far
    I have added a function to conveniently print tilesets to the console.
    I have also fixed the formatting of some of the Tileset Data.gd code.

--- Tileset Things/Tileset Data.gd
+++ Tileset Things/Tileset Data.gd
@@ -9,11 +9,28 @@ const BASE_TILE: int = 0
 
 var empty_tset: Array[Array]:
 	get:
-		var new_tset:Array[Array]
-		var new_row: Array[int]
+		var new_tset: Array[Array] = []
+		var new_row: Array[int] = []
 		new_row.resize(COLUMNS)
 		new_row.fill(BASE_TILE)
 		new_tset.resize(ROWS)
 		new_tset.fill(new_row)
 		return new_tset
 #endregion Tileset Creation
+
+#region TEST
+func print_tset(tset):
+	var array_string: String = str(tset)
+	var tx: String = " "
+	var bra: bool = false
+	for ch in array_string:
+		tx = tx + ch
+		if (bra == true && ch == ","):
+			print(tx)
+			tx = " "
+		bra = true if ch == "]" else false
+	print(tx)
+
+func _ready():
+	print_tset(empty_tset)
+#endregion TEST
